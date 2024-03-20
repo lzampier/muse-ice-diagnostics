9d87bedb29ccf14788e8e6c523d8ba501d6621f5
Lorenzo Zampieri <zampieri@administrators-MacBook-Pro-3.local>
2024-03-20 17:18:31 +0100

    .gitignore and basic package structure has been added

---
+++ musevice/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of pyfesom2
+# Original code by Dmitry Sidorenko, Qiang Wang, Sergey Danilov and Patrick Scholz
+#
+
+"""Top-level package for musevice."""
+
+__author__ = """MUSE ice team"""
+__email__ = "lorenzo.zampieri@cmcc.it"
+__version__ = "0.0.1"
+
+from .generate_ux_mesh import *
